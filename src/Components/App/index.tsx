e0656f30a95f8c54d5d1861b07fbda81d07d359c
HamSungJun <tjdwns5123@gmail.com>
2022-01-02 16:04:46 +0900

    ADD: Recoil 라이브러리 추가, Eslint 설정 변경, NotFound 페이지 추가

--- src/Components/App/index.tsx
+++ src/Components/App/index.tsx
@@ -1,11 +1,16 @@
 import { StrictMode } from 'react';
+import {
+  RecoilRoot, atom, selector, useRecoilState, useRecoilValue,
+} from 'recoil';
 import Router from '@/Router';
 import './index.scss';
 
 export default function App() {
   return (
     <StrictMode>
-      <Router />
+      <RecoilRoot>
+        <Router />
+      </RecoilRoot>
     </StrictMode>
   );
 }
