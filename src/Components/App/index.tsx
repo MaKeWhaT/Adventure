ac2c67088f55099940edc17a427df88d26bca254
HSJPRIME <tjdwns5123@gmail.com>
2022-04-04 01:21:53 +0900

    [AD-55] Vite Bundle Analyzer 추가 (#40)

--- src/Components/App/index.tsx
+++ src/Components/App/index.tsx
@@ -1,14 +1,11 @@
 import { StrictMode } from "react";
-import { RecoilRoot } from "recoil";
 import Router from "@/Router";
 import "./index.scss";
 
 export default function App() {
   return (
     <StrictMode>
-      <RecoilRoot>
-        <Router />
-      </RecoilRoot>
+      <Router />
     </StrictMode>
   );
 }
