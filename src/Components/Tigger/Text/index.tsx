d5b61f2be8d0a5495b9ae18bcf3f9f9446c7a514
cansoup <0311minha@gmail.com>
2022-04-04 22:56:58 +0900

    0404: text component

--- src/Components/Tigger/Text/index.tsx
+++ src/Components/Tigger/Text/index.tsx
@@ -9,21 +9,23 @@ import { ITextTigger } from "./types";
  *  line-height
  *  //font-variant 소문자를 작은 대문자로 변경할 수 있는 속성. 한글에서는 쓰이지 않는다.
  */
-export default function TextTigger( {
+export default function TextTigger({
   customTag = "span",
   className,
   styleFont,
   children,
-}: ITextTigger ) {
+}: ITextTigger) {
   const Tag = `${customTag}` as keyof JSX.IntrinsicElements;
   const textStyle = {
     color: styleFont?.color,
     fontStyle: styleFont?.fontStyle,
     fontFamily: styleFont?.fontStyle,
     fontWeight: styleFont?.fontWeight,
-    lineHeight: styleFont?.lineHeight
-  }
+    lineHeight: styleFont?.lineHeight,
+  };
   return (
-    <Tag className={className} style={textStyle}>{children}</Tag>
+    <Tag className={className} style={textStyle}>
+      {children}
+    </Tag>
   );
 }
