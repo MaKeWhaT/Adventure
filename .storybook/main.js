7a1d9cd13595902b2550bed0da1428a8795f5a32
kevin.sjj <kevin.sjj@kakaoent.com>
2022-05-31 00:51:08 +0900

    storybook upgrade & use turbo build addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,46 @@
+const path = require("path");
+const merge = require("webpack-merge").merge;
+const webpack = require("webpack");
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    // {
+    //   name: "storybook-addon-turbo-build",
+    //   options: {
+    //     optimizationLevel: 3,
+    //     disableSourceMap: true,
+    //   },
+    // },
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  framework: "@storybook/react",
+  features: {
+    storyStoreV7: true,
+  },
+  core: {
+    builder: "@storybook/builder-webpack5",
+    options: {
+      lazyCompilation: true,
+      fsCache: true,
+    },
+  },
+  webpackFinal: async (config) => {
+    return merge(config, {
+      resolve: {
+        alias: {
+          "@": path.resolve(__dirname, "../src"),
+          pages: path.resolve(__dirname, "../pages"),
+          Components: path.resolve(__dirname, "../src", "Components"),
+        },
+      },
+      plugins: [
+        new webpack.ProvidePlugin({
+          React: "react",
+        }),
+      ],
+    });
+  },
+};
